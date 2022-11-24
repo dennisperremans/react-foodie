0f66cf6a209a446de085624e0fbf75738f81b584
Dennis Perremans <perremansdennis@gmail.com>
2022-11-24 21:52:02 +0100

    added the cuisine page + styled component category

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -6,23 +6,23 @@ import React from 'react'
 
 function Category() {
   return (
-    <List>
-        <NavLink to={'/cuisine/Italian'}>
+    <List className="navigation">
+        <StyledLink className="nav-item" to={'/cuisine/Italian'}>
             <FaPizzaSlice />
             <h4>Italian</h4>
-        </NavLink>
-        <NavLink to={'/cuisine/American'}>
+        </StyledLink>
+        <StyledLink className="nav-item" to={'/cuisine/American'}>
             <FaHamburger />
             <h4>American</h4>
-        </NavLink>
-        <NavLink to={'/cuisine/Thai'}>
+        </StyledLink>
+        <StyledLink className="nav-item" to={'/cuisine/Thai'}>
             <GiNoodles />
             <h4>Thai</h4>
-        </NavLink>
-        <NavLink to={'/cuisine/Japanse'}>
+        </StyledLink>
+        <StyledLink className="nav-item" to={'/cuisine/Japanese'}>
             <GiChopsticks />
             <h4>Japanese</h4>
-        </NavLink>
+        </StyledLink>
     </List>
   )
 }
@@ -32,5 +32,43 @@ const List = styled.div`
     justify-content: center;
     margin: 2rem 0;
 `
+
+const StyledLink = styled(NavLink)`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 0 1rem;
+    text-align: center;
+    flex-direction: column;
+    text-decoration: none;
+
+    svg {
+        background-color: #333;
+        color: #FFF;
+        border-radius: 50%;
+        width: 50px;
+        height: 50px;
+        padding: 1rem;
+        transition: all .5s;
+    }
+
+    h4 {
+        color: #AAA;
+        font-size: .75rem;
+        margin-top: .5rem;
+        transition: all .5s;
+    }
+
+    &:hover {
+        svg {
+            background-color: #111;
+        }
+
+        h4 {
+            color: #111;
+        }
+    }
+`
+
 
 export default Category
